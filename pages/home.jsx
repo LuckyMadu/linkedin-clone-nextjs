c5d9b62aa17191bdb19df321e17163a8ee4fe35b
Lahiru Amaratunge <lahiru.a@keeneye.solutions>
2022-01-22 23:34:46 +0530

    login header styles

---
+++ pages/home.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/alt-text */
+import Image from "next/image";
+import HeaderLink from "../components/HeaderLink";
+import ExploreIcon from "@mui/icons-material/Explore";
+import GroupIcon from "@mui/icons-material/Group";
+import OndemandVideoSharpIcon from "@mui/icons-material/OndemandVideoSharp";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ArrowForwardIosRoundedIcon from "@mui/icons-material/ArrowForwardIosRounded";
+
+const Home = () => {
+  return (
+    <div>
+      <header className="flex justify-around items-center py-4">
+        <div className="relative w-36 h-10">
+          <Image src="https://rb.gy/vtbzlp" layout="fill" objectFit="contain" />
+        </div>
+        <div className="flex items-center sm:divide-x divide-gray-300">
+          <div className="hidden sm:flex space-x-8 pr-4">
+            <HeaderLink Icon={ExploreIcon} text="Discover" />
+            <HeaderLink Icon={GroupIcon} text="People" />
+            <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" />
+            <HeaderLink Icon={BusinessCenterIcon} text="Jobs" />
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
